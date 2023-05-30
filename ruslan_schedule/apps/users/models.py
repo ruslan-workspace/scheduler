19a0d143aa4f0017974dfd8347835a642c30c017
Anzor <magometov_a.z@mail.ru>
2023-05-31 00:22:22 +0300

    create User model

---
+++ ruslan_schedule/apps/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, primary_key=True)
+    telegram_id = models.PositiveIntegerField(blank=True, null=True, unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.username} | {self.email} | {self.get_full_name()} "
