c43b4405e2ce2430758b1dd0743af52834fe5346
Anzor <100163663+Magometov@users.noreply.github.com>
2023-06-20 20:22:36 +0300

    add invitations (#2)

--- src/apps/events/models.py
+++ src/apps/events/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
-from src.apps.users.models import User
-
 
 class Event(TimeStampedModel):
     title = models.CharField(max_length=255)
     start = models.DateTimeField()
     end = models.DateTimeField()
     owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="created_events"
+        "users.User", on_delete=models.CASCADE, related_name="created_events"
     )
     description = models.TextField(blank=True)
     meeting_link = models.URLField(blank=True, null=True)
+    guests = models.ManyToManyField("users.User", through="invitations.Invitation")
